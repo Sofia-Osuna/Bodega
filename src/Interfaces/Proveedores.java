d20478a20ea35276bdb62bf4b031b75ad9c799b3
sofiaosuna <sofiaosuna@localhost>
2025-07-31 00:42:50 -0700

    arreglos menores

--- src/Interfaces/Proveedores.java
+++ src/Interfaces/Proveedores.java
@@ -226,6 +226,7 @@ public class Proveedores extends javax.swing.JFrame {
 
         jButton1.setBackground(new java.awt.Color(42, 138, 127));
         jButton1.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jButton1.setForeground(new java.awt.Color(255, 255, 255));
         jButton1.setText("Nuevo proveedor");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
